4c0cdc0243766d3c44e14a4237f1ee50ec1b23c8
Igor Motyka <igor_motyka@mail.com>
2020-02-22 23:29:50 +0100

    Fixed header mobile placment

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,33 @@ const HeaderWrapper = styled.div`
   left: 50%;
   transform: translate(-50%, 0);
   opacity: 0;
+  width: 100%;
   h1 {
-    font-size: 5rem;
+    font-size: 4rem;
   }
   p {
-    margin-top: 4rem;
+    margin-top: 2rem;
     font-size: 1.5rem;
     font-weight: lighter;
+  }
+  @media (min-width: ${props => props.theme.sm}) {
+    h1 {
+      font-size: 5rem;
+    }
+    p {
+      margin-top: 4rem;
+      font-size: 1.5rem;
+      font-weight: lighter;
+    }
   }
 `
 const Icons = styled.div`
   margin-top: 3rem;
+  .icon {
+    margin: 15px;
+  }
+  @media (min-width: ${props => props.theme.sm}) {
+  }
 `
 export default class Header extends React.Component {
   constructor(props) {
@@ -41,7 +57,6 @@ export default class Header extends React.Component {
         <IconContext.Provider
           value={{
             size: "35px",
-            style: { margin: "2rem" },
             className: "icon",
           }}
         >

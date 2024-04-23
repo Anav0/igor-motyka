e42f58c16c263b8e665c24a5df75602579072be2
Igor Motyka <igormotyka6@gmail.com>
2024-04-23 02:04:58 +0200

    Bring back Icons

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { FaRegEnvelope, FaGithub, FaBehance, FaLinkedin } from "react-icons/fa"
+import { IconContext } from "react-icons"
 import { HeaderWrapper, Icons } from "./header.styled"
 
 export default class Header extends React.Component {
@@ -11,29 +13,39 @@ export default class Header extends React.Component {
       <HeaderWrapper>
         <h1>Igor Motyka</h1>
         <p>Fullstack developer</p>
+        <IconContext.Provider
+          value={{
+            size: "35px",
+            className: "icon",
+          }}
+        >
           <Icons>
             <a href="mailto:igor_motyka@mail.com">
+              <FaRegEnvelope />
             </a>
             <a
               target="_blank"
               rel="noreferrer noopener"
               href="https://github.com/Anav0"
             >
+              <FaGithub />
             </a>{" "}
             <a
               target="_blank"
               rel="noreferrer noopener"
               href="https://www.linkedin.com/in/%E1%9B%9D-igor-m-873439168/"
             >
+              <FaLinkedin />
             </a>
             <a
               target="_blank"
               rel="noreferrer noopener"
               href="https://www.behance.net/igoranawski"
             >
-              <span>Behance</span>
+              <FaBehance />
             </a>
           </Icons>
+        </IconContext.Provider>
       </HeaderWrapper>
     )
   }
--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { TweenMax, TimelineMax, Power2 } from "gsap"
 import * as ScrollMagic from "scrollmagic"
+import { IconContext } from "react-icons"
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap"
 import { GatsbyImage, StaticImage } from "gatsby-plugin-image"
 import {
@@ -11,12 +12,19 @@ import {
   Stack,
   StackIcons,
 } from "./project.styled"
+import {
+  FaPython,
+  FaReact,
+  FaSass,
+  FaVuejs,
+  FiArrowUpRight,
+} from "react-icons/all"
 
 const icons = {
-  vue: (key) => <span key={key} >Vue</span>,
-  scss: (key) => <span key={key} >Scss</span>,
-  react: (key) => <span key={key} >React</span>,
-  python: (key) => <span key={key} >Python</span>,
+  vue: (key) => <FaVuejs key={key} />,
+  scss: (key) => <FaSass key={key} />,
+  react: (key) => <FaReact key={key} />,
+  python: (key) => <FaPython key={key} />,
 }
 
 export default ({ project, image }) => {
@@ -78,12 +86,24 @@ export default ({ project, image }) => {
       <ProjectDesc ref={projectDesc}>
         <h1>{project.title}</h1>
         <p>{project.desc}</p>
+        <IconContext.Provider
+          value={{
+            size: "35px",
+            style: { margin: "24px" },
+            className: "icon",
+          }}
+        >
           <a target="_blank" rel="noreferrer noopener" href={project.url}>
-            <span>ICON</span>
+            <FiArrowUpRight />
           </a>
-        
+        </IconContext.Provider>
         <Stack>
           <h5 className="stack-header">Stack:</h5>
+          <IconContext.Provider
+            value={{
+              size: "24px",
+            }}
+          >
             <StackIcons>
               {project.stack.map((element) => {
                 if (icons[element])
@@ -92,6 +112,7 @@ export default ({ project, image }) => {
                   return <span key={project.id + element}>{element}</span>
               })}
             </StackIcons>
+          </IconContext.Provider>
         </Stack>
       </ProjectDesc>
     </ProjectWrapper>
